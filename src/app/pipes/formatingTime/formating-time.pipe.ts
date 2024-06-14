0598805e36579332b37428dc41a552cf4e57b574
Valeria <perkayay@gmail.com>
2024-06-14 21:53:24 +0300

    feat: new pipe

---
+++ src/app/pipes/formatingTime/formating-time.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'formatingTime',
+  standalone: true,
+})
+export class FormatingTimePipe implements PipeTransform {
+  transform(value: number): string {
+    const hours = Math.floor(value / 60);
+    const remainingMinutes = value % 60;
+    const formattedHours = hours.toString().padStart(2, '0');
+    const formattedMinutes = remainingMinutes.toString().padStart(2, '0');
+    return `${formattedHours}:${formattedMinutes}`;
+  }
+}
