d03a7ae216f379fb5c36d77cca6cd79417e7470b
inga-lekhman <inga.lekhman@gmail.com>
2024-06-19 19:10:12 +0300

    other approach

--- src/app/components/movie-card/movie-card.component.ts
+++ src/app/components/movie-card/movie-card.component.ts
@@ -13,14 +13,21 @@ import { ButtonModule } from 'primeng/button';
 })
 export class MovieCardComponent {
   @Input() movie: any;
+  @Input() isFavorite = false;
+  @Input() isInWatchList = false;
 
   @Output() addFavorite = new EventEmitter();
   @Output() addWatchList = new EventEmitter();
 
+
   addToFavorite() {
+    this.isFavorite = !this.isFavorite
     this.addFavorite.emit(this.movie);
+    
   }
   addToWatchList() {
+    this.isInWatchList = !this.isInWatchList
     this.addWatchList.emit(this.movie);
+  
   }
 }
--- src/app/components/movie-list/movie-list.component.ts
+++ src/app/components/movie-list/movie-list.component.ts
@@ -18,20 +18,16 @@ export class MovieListComponent {
     const movieIndex = this.favoriteList.findIndex((obj) => obj === movie);
     if (movieIndex !== -1) {
       this.favoriteList.splice(movieIndex, 1);
-      movie.addedToFav = false;
     } else {
       this.favoriteList.push(movie);
-      movie.addedToFav = true;
     }
   }
   handleWatchList(movie: any) {
     const movieIndex = this.watchLaterList.findIndex((obj) => obj === movie);
     if (movieIndex !== -1) {
       this.watchLaterList.splice(movieIndex, 1);
-      movie.addedToWatch = false;
     } else {
       this.watchLaterList.push(movie);
-      movie.addedToWatch = true;
     }
   }
 }
