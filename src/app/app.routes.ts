b6b144c1cd7dec1692ad7a7ad8255d5454e8ef7f
Valeria <perkayay@gmail.com>
2024-09-18 17:04:50 +0300

    refactor: remove all unnecessary resolvers

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,11 +7,6 @@ import { MoviePopularPageComponent } from './pages/movie-popular-page/movie-popu
 import { MovieUpcomingPageComponent } from './pages/movie-upcoming-page/movie-upcoming-page.component';
 import { FavoriteMoviesPageComponent } from './pages/favorite-movies-page/favorite-movies-page.component';
 import { WatchLaterPageComponent } from './pages/watch-later-page/watch-later-page.component';
-import { PopularMoviesResolver } from './guards/popular.resolver';
-import { MovieListResolver } from './guards/movie-list.resolver';
-import { NowPlayingMoviesResolver } from './guards/now-playing.resolver';
-import { TopRatedMoviesResolver } from './guards/top-rated.resolver';
-import { UpcomingMoviesResolver } from './guards/upcoming.resolver';
 import { MovieGuard } from './guards/movie.guard';
 import { SearchMoviePageComponent } from './pages/search-movie-page/search-movie-page.component';
 
@@ -19,37 +14,22 @@ export const routes: Routes = [
   {
     path: '',
     component: MovieListPageComponent,
-    resolve: {
-      movieData: MovieListResolver,
-    },
   },
   {
     path: 'now-playing',
     component: MovieNowPlayingPageComponent,
-    resolve: {
-      movieData: NowPlayingMoviesResolver,
-    },
   },
   {
     path: 'top-rated',
     component: MovieNowTopRatePageComponent,
-    resolve: {
-      movieData: TopRatedMoviesResolver,
-    },
   },
   {
     path: 'popular',
     component: MoviePopularPageComponent,
-    resolve: {
-      movieData: PopularMoviesResolver,
-    },
   },
   {
     path: 'upcoming',
     component: MovieUpcomingPageComponent,
-    resolve: {
-      movieData: UpcomingMoviesResolver,
-    },
   },
   { path: 'movie/:id', component: MovieDetailsPageComponent },
   {
@@ -62,7 +42,5 @@ export const routes: Routes = [
     component: WatchLaterPageComponent,
     canActivate: [MovieGuard],
   },
-  {path: 'search',
-    component: SearchMoviePageComponent
-  }
+  { path: 'search', component: SearchMoviePageComponent },
 ];
