7ad1844524d9acacec116c1f5a900eb68da12d93
Valeria <perkayay@gmail.com>
2024-10-03 18:25:30 +0300

    refactor: abstract class for unsubscribe

--- src/app/pages/movie-details-page/movie-details-page.component.ts
+++ src/app/pages/movie-details-page/movie-details-page.component.ts
@@ -4,11 +4,12 @@ import { MovieCardComponent } from '../../components/movie-card/movie-card.compo
 import { ActivatedRoute, Route } from '@angular/router';
 import { MathRoundPipe } from '../../pipes/mathRound/math-round.pipe';
 import { FormatingTimePipe } from '../../pipes/formatingTime/formating-time.pipe';
-import { skip, Subscription } from 'rxjs';
+import { skip, Subscription, takeUntil } from 'rxjs';
 import { MovieDetails } from '../../models/movie-details.model';
 import { Store } from '@ngrx/store';
 import { loadMovieById } from '../../store/movie-store/actions';
 import { selectMovieById } from '../../store/movie-store/selectors';
+import { ClearObservable } from '../../directives/clear-observable.directive';
 
 @Component({
   selector: 'app-movie-details-page',
@@ -17,37 +18,31 @@ import { selectMovieById } from '../../store/movie-store/selectors';
   styleUrl: './movie-details-page.component.scss',
   imports: [CommonModule, MovieCardComponent, MathRoundPipe, FormatingTimePipe],
 })
-export class MovieDetailsPageComponent implements OnInit, OnDestroy {
-  private movieSubscribtion: Subscription = new Subscription();
-  private routeSubscribtion: Subscription = new Subscription();
+export class MovieDetailsPageComponent
+  extends ClearObservable
+  implements OnInit, OnDestroy
+{
   movieData: MovieDetails | null = null;
   STATIC_IMAGE_PATH: string = 'https://image.tmdb.org/t/p/w500/';
   incomingMovieId: number = 0;
 
-  constructor(private route: ActivatedRoute, private store: Store) {}
+  constructor(private route: ActivatedRoute, private store: Store) {
+    super();
+  }
 
   ngOnInit(): void {
-    this.routeSubscribtion = this.route.params.subscribe((data) => {
+    this.route.params.pipe(takeUntil(this.destroy$)).subscribe((data) => {
       this.incomingMovieId = Number(data['id']);
       this.store.dispatch(loadMovieById({ id: this.incomingMovieId }));
     });
 
-    this.movieSubscribtion = this.store
+    this.store
       .select(selectMovieById)
-      .pipe(skip(1))
+      .pipe(skip(1), takeUntil(this.destroy$))
       .subscribe((movie) => {
         if (movie) {
           this.movieData = movie;
         }
       });
-  }
-
-  ngOnDestroy(): void {
-    if (this.movieSubscribtion) {
-      this.movieSubscribtion.unsubscribe();
-    }
-    if (this.routeSubscribtion) {
-      this.routeSubscribtion.unsubscribe();
-    }
   }
 }
