bb8d716301c5bf95c105642647c2059a39aa134a
Valeria <perkayay@gmail.com>
2025-02-12 14:53:39 +0200

    1 - create general comp for refactor pages

---
+++ src/app/pages/movies-page/movies-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movies-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './movies-page.component.html',
+  styleUrl: './movies-page.component.scss'
+})
+export class MoviesPageComponent {
+
+}
