f6358fbdcccbe481a6925ebf4fd1242ec4d9b70d
Valeria <perkayay@gmail.com>
2025-02-18 13:46:27 +0200

    1 - delete all unnessecery comps

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,8 @@
 import { Routes } from '@angular/router';
 import { MovieDetailsPageComponent } from './pages/movie-details-page/movie-details-page.component';
-import { MovieListPageComponent } from './pages/movie-list-page/movie-list-page.component';
-import { MovieNowPlayingPageComponent } from './pages/movie-now-playing-page/movie-now-playing-page.component';
-import { MovieNowTopRatePageComponent } from './pages/movie-now-top-rate-page/movie-now-top-rate-page.component';
-import { MoviePopularPageComponent } from './pages/movie-popular-page/movie-popular-page.component';
-import { MovieUpcomingPageComponent } from './pages/movie-upcoming-page/movie-upcoming-page.component';
 import { FavoriteMoviesPageComponent } from './pages/favorite-movies-page/favorite-movies-page.component';
 import { WatchLaterPageComponent } from './pages/watch-later-page/watch-later-page.component';
 import { MovieGuard } from './guards/movie.guard';
-import { SearchMoviePageComponent } from './pages/search-movie-page/search-movie-page.component';
 import { MoviesPageComponent } from './pages/movies-page/movies-page.component';
 
 export const routes: Routes = [
@@ -27,7 +21,6 @@ export const routes: Routes = [
     component: WatchLaterPageComponent,
     canActivate: [MovieGuard],
   },
-  { path: 'search', component: SearchMoviePageComponent },
   {
     path: ':category',
     component: MoviesPageComponent,
