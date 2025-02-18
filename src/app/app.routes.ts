03bd5415ead512cbad4eb9403e79f372ce6c7390
Valeria <perkayay@gmail.com>
2025-02-18 15:12:12 +0200

    2 - remove guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { MovieDetailsPageComponent } from './pages/movie-details-page/movie-details-page.component';
 import { FavoriteMoviesPageComponent } from './pages/favorite-movies-page/favorite-movies-page.component';
 import { WatchLaterPageComponent } from './pages/watch-later-page/watch-later-page.component';
-import { MovieGuard } from './guards/movie.guard';
 import { MoviesPageComponent } from './pages/movies-page/movies-page.component';
 
 export const routes: Routes = [
@@ -14,12 +13,10 @@ export const routes: Routes = [
   {
     path: 'favorite',
     component: FavoriteMoviesPageComponent,
-    canActivate: [MovieGuard],
   },
   {
     path: 'watch-later',
     component: WatchLaterPageComponent,
-    canActivate: [MovieGuard],
   },
   {
     path: ':category',
