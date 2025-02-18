437f4b8ca3ef4e30ca1ab9e5e350d7598d6a8f67
Valeria <perkayay@gmail.com>
2025-02-18 17:00:35 +0200

    2 - fix logic and add new one for unlogged users

--- src/app/components/movie-header/movie-header.component.ts
+++ src/app/components/movie-header/movie-header.component.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { MovieSidebarComponent } from '../movie-sidebar/movie-sidebar.component';
 import { movieSelectedListType } from './movie-lists.type';
 import { Store } from '@ngrx/store';
 import { selectAccountId } from '../../store/auth-store/selectors';
-import { take } from 'rxjs';
+import { filter, map, switchMap, take, takeUntil } from 'rxjs';
 import { showAuthPopup } from '../../store/auth-store/actions';
+import { ClearObservable } from '../../directives/clear-observable.directive';
 
 @Component({
   selector: 'app-movie-header',
@@ -16,19 +17,40 @@ import { showAuthPopup } from '../../store/auth-store/actions';
   templateUrl: './movie-header.component.html',
   styleUrl: './movie-header.component.scss',
 })
-export class MovieHeaderComponent implements OnInit {
-  constructor(private authStore: Store, private router: Router) {}
+export class MovieHeaderComponent extends ClearObservable implements OnInit {
+  constructor(private authStore: Store, private router: Router) {
+    super();
+  }
 
-  private isLoggedIn: string | null = null;
+  private isLoggedIn: boolean = false;
+  private currentPage: string = '';
 
   ngOnInit(): void {
-    this.authStore
-      .select(selectAccountId)
-      .pipe(take(1))
+    this.router.events
+      .pipe(
+        takeUntil(this.destroy$),
+        filter((event: any) => event instanceof NavigationEnd),
+        map((event: NavigationEnd) => event.url),
+        switchMap((currentPage) => {
+          this.currentPage = currentPage;
+          return this.authStore.select(selectAccountId);
+        })
+      )
       .subscribe((accountId: string | null) => {
-        this.isLoggedIn = accountId;
+        this.isLoggedIn = !!accountId;
+
+        if (
+          !this.isLoggedIn &&
+          (this.currentPage === '/favorite' ||
+            this.currentPage === '/watch-later')
+        ) {
+          this.authStore.dispatch(
+            showAuthPopup({ redirectUrl: `${this.currentPage}` })
+          );
+        }
       });
   }
+
   openSelectedList(type: movieSelectedListType): void {
     if (this.isLoggedIn) {
       this.router.navigate([`/${type}`]);
