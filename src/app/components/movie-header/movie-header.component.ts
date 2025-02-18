6970605987fce0e813fe07ccb0d7313adcb7b07b
Valeria <perkayay@gmail.com>
2025-02-18 15:13:04 +0200

    2 - add new logic to guard and redirect user

--- src/app/components/movie-header/movie-header.component.ts
+++ src/app/components/movie-header/movie-header.component.ts
@@ -1,8 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { MovieSidebarComponent } from '../movie-sidebar/movie-sidebar.component';
+import { movieSelectedListType } from './movie-lists.type';
+import { Store } from '@ngrx/store';
+import { selectAccountId } from '../../store/auth-store/selectors';
+import { take } from 'rxjs';
+import { showAuthPopup } from '../../store/auth-store/actions';
 
 @Component({
   selector: 'app-movie-header',
@@ -11,6 +16,24 @@ import { MovieSidebarComponent } from '../movie-sidebar/movie-sidebar.component'
   templateUrl: './movie-header.component.html',
   styleUrl: './movie-header.component.scss',
 })
-export class MovieHeaderComponent {
-  constructor() {}
+export class MovieHeaderComponent implements OnInit {
+  constructor(private authStore: Store, private router: Router) {}
+
+  private isLoggedIn: string | null = null;
+
+  ngOnInit(): void {
+    this.authStore
+      .select(selectAccountId)
+      .pipe(take(1))
+      .subscribe((accountId: string | null) => {
+        this.isLoggedIn = accountId;
+      });
+  }
+  openSelectedList(type: movieSelectedListType): void {
+    if (this.isLoggedIn) {
+      this.router.navigate([`/${type}`]);
+    } else {
+      this.authStore.dispatch(showAuthPopup({ redirectUrl: `/${type}` }));
+    }
+  }
 }
