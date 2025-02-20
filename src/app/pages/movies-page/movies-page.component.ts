2b605933e55140f50d57fe0b7d1e3834512b6016
Valeria <perkayay@gmail.com>
2025-02-20 17:52:40 +0200

    3- code sty;e fixes

--- src/app/pages/movies-page/movies-page.component.ts
+++ src/app/pages/movies-page/movies-page.component.ts
@@ -12,8 +12,8 @@ import {
   selectUpcomingMovies,
 } from '../../store/movie-store/selectors';
 import { Movie, movieLoadingState } from '../../models/movie.model';
-import { MovieCardComponent } from "../../components/movie-card/movie-card.component";
-import { LoadingCardComponent } from "../../components/loading-card/loading-card.component";
+import { MovieCardComponent } from '../../components/movie-card/movie-card.component';
+import { LoadingCardComponent } from '../../components/loading-card/loading-card.component';
 
 @Component({
   selector: 'app-movies-page',
@@ -28,7 +28,7 @@ export class MoviesPageComponent extends ClearObservable implements OnInit {
 
   pageTitle = '';
   loadingCardsAmount = Array(5);
-  loadingState: movieLoadingState = 'loading'
+  loadingState: movieLoadingState = 'loading';
   private listsNamesAndTriggers = new Map<string, any>([
     ['all-movies', selectAllMovies],
     ['upcoming', selectUpcomingMovies],
@@ -39,12 +39,12 @@ export class MoviesPageComponent extends ClearObservable implements OnInit {
   ]);
 
   private categoryTitles: { [key: string]: string } = {
-    'upcoming': "Upcoming",
-    'all-movies': "All Movies",
+    upcoming: 'Upcoming',
+    'all-movies': 'All Movies',
     'now-playing': 'Now playing',
     'top-rated': 'Top Rated',
     popular: 'Popular',
-    search: 'Searching Movies'
+    search: 'Searching Movies',
   };
 
   constructor(private route: ActivatedRoute, private store: Store) {
@@ -62,10 +62,10 @@ export class MoviesPageComponent extends ClearObservable implements OnInit {
   private getCurrentMovieList(category: string): void {
     let selector = this.listsNamesAndTriggers.get(category);
     if (!category) {
-      selector = this.listsNamesAndTriggers.get('all-movies')
+      selector = this.listsNamesAndTriggers.get('all-movies');
     }
 
-    this.loadingState = 'loading'
+    this.loadingState = 'loading';
 
     if (selector) {
       this.store
@@ -74,9 +74,9 @@ export class MoviesPageComponent extends ClearObservable implements OnInit {
         .subscribe((movieList: any) => {
           if (movieList && movieList.length > 0) {
             this.currentMovieList = movieList;
-            this.loadingState = 'loaded'
-          }else{
-            this.loadingState = 'error'
+            this.loadingState = 'loaded';
+          } else {
+            this.loadingState = 'error';
           }
         });
     }
@@ -84,8 +84,8 @@ export class MoviesPageComponent extends ClearObservable implements OnInit {
 
   private getTitleForCategory(category: string): void {
     if (!category) {
-      this.pageTitle = this.categoryTitles['all-movies']
-      return
+      this.pageTitle = this.categoryTitles['all-movies'];
+      return;
     }
     this.pageTitle = this.categoryTitles[category];
   }
